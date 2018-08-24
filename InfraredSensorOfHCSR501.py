fd04b73f078f0c1e66828498ee5a29f94e01c36d
wuyiliang <wuyiliang@e-lead.cn>
2018-08-25 00:25:40 +0800

    补充备注

--- InfraredSensorOfHCSR501.py
+++ InfraredSensorOfHCSR501.py
@@ -1,19 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
+#设定红外传感器和LED灯引脚
+SENSOR_PIN=7
+LED_PIN=12
+
+#初始化
 def init():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(7,GPIO.IN)
+    GPIO.setup(SENSOR_PIN,GPIO.IN) # 红外传感器
+    GPIO.setup(LED_PIN,GPIO.OUT) # LED二极管
     pass
 
+#检测函数
 def detct():
     for i in range(1, 31):
-        if GPIO.input(7) == True:
-            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "  Someone is closing!")
-         else:
+        if GPIO.input(SENSOR_PIN) == True:#检测到信号
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "  Someone is closing!") #打印语句
+            GPIO.output(LED_PIN,True)#检测到信号灯亮
+        else:
             print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "  Noanybody!")
-        time.sleep(6)  # 每6秒检查一次
 
-time.sleep(2)
+        time.sleep(3)  # 每6秒检查一次
+
+        if GPIO.input(LED_PIN) == True:
+            GPIO.output(LED_PIN, False)
+            print("led is off")
+
+#执行
 init()
 detct()
+GPIO.cleanup()
